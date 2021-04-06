69fd13cf2d158da7aa613c9cc727b11e1c24c119
Mojtaba Hafezi <20971735+MojtabaHafezi@users.noreply.github.com>
2021-04-06 09:30:36 +0200

    feat: health bar
    + adds a UI health bar

---
+++ Assets/Scripts/Player/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Slider mySlider;
+
+    private void Awake()
+    {
+        mySlider = GetComponent<Slider>();
+    }
+
+    public void SetHealth(int health)
+    {
+        mySlider.value = health;
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        mySlider.maxValue = health;
+        mySlider.value = health;
+    }
+}
